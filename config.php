656317874f92479eb5353da5362445be6927a79c
Edujvr <32462670+Edujvr@users.noreply.github.com>
2018-10-08 20:28:59 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,16 +3,17 @@ error_reporting(0);
 
 
 // Inserisci il dominio del tuo sito, dove avrai creato la cartella che contiene il file index.php sul tuo server.
-$MAIN_URL = 'https://boton-reserva-php.herokuapp.com/index.php';
+$MAIN_URL = 'https://botonfecha.000webhostapp.com/index.php';
 
 // Inserisci il tuo Broadcasting API Token: lo trovi nel tab CONFIGURE del tuo bot in ChatFuel.
-$CHATFUEL_BROADCAST_TOKEN = 'mELtlMAHYqR0BvgEiMq8zVek3uYUK3OJMbtyrdNPTrQB9ndV0fM7lWTFZbM4MZvD';
+$CHATFUEL_BROADCAST_TOKEN = 'qwYLsCSz8hk4ytd6CPKP4C0oalstMnGdpDjF8YFHPHCieKNc0AfrnjVs91fGuH74';
 
 // Inserisci l'ID del tuo Bot: lo trovi nella url del browser, subito dopo /bot/
-$CHATFUEL_BOT_ID = '5b872a7e76ccbc23f94a3bc1';
+$CHATFUEL_BOT_ID = '5b908e3676ccbc75afd56f45';
 
 // Il nome del blocco verso cui indirizzare l'utente dopo il click sulla data: non modificarlo.
 $CHATFUEL_RETURN_BLOCK_NAME = 'blocco_di_ritorno';
 
 
 ?>
+
